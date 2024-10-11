ad42abf79676ca475b660a21fed49ca0cc3b0cdb
valeriy <valeriy>
2024-10-11 11:23:32 +0300

    add: create car object

---
+++ object.ts
@@ -0,0 +1,16 @@
+const machineСharacteristics ={
+    salon:'светлый натуральный кожа',
+    assistanceSystems: 'Датчик дождя',
+    exterior:'Alloy wheels',
+    interior:'Люк',
+    headlights:'Xenon',
+    climate:'Climate control',
+    heating:'Seats',
+    comfort:'Автозапуск двигателя',
+    multimedia:'Bluetooth',
+       
+} 
+
+machineСharacteristics.salon = 'светлый натуральный кожа'
+
+ console.log(machineСharacteristics)
