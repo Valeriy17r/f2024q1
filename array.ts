769b2008e0f529d9f9f9d5d2589895f950ba7d89
valeriy <valeriy>
2024-10-18 11:39:17 +0300

    add: array

---
+++ array.ts
@@ -0,0 +1,6 @@
+ const fruits = ['apple', 'banana', 'grapes','kiwi','orange']
+const vegetables = ['cucumber','carrots','potatoes','beet','pumpkin']
+fruits.push('pomidur')
+const plants = fruits.concat(vegetables)
+plants.sort()
+ console.log(plants)
