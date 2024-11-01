235e7bf9845a6a86b3079a02d2a89a01fda46aa1
valeriy <valeriy>
2024-11-01 11:41:54 +0300

    add: musicFunction

---
+++ Forarray.ts
@@ -0,0 +1,10 @@
+const arrayflower =['tulip', 'cornflower','daisy','rose','carnation']
+for ( let y = 0; y < arrayflower.length; y++);{
+     if(arrayflower[y] === 'tulip'){
+          console.log('в данном массиве найден ${flowerArray[y]}')
+     }
+}
+
+
+
+arrayflower[0]
