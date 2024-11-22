1126118cd43ed0a5e1578285d7f668e6ef4f9606
valeriy <valeriy>
2024-11-22 11:42:41 +0300

    add:sort.js

---
+++ sort.js
@@ -0,0 +1,17 @@
+const words = ['car','rabbit','apple', 'rad', 'pig','ariadna', 'fox','dad','mum','sister']
+const resultUp = words.sort()
+console.log(words)
+
+
+const array3 = [2,5,15,9,3,87345];
+array3.sort();
+console.log(array3);
+
+const words = ['car','rabbit','apple', 'rad', 'pig','ariadna', 'fox','dad','mum','sister']
+const resultUp = word.sort((a,b)=> a-b)
+ //console.log(resultUp)
+
+const resultDown = word.sort((a,b)=> a-b)
+resultDown.reverse()
+
+console.log(resultDown)
